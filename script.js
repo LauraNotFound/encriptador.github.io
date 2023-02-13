4c1355189628b7c50607737e349f4b9e30a658a3
Laura <lcmm97.lcmm@gmail.com>
2023-02-13 18:54:33 -0500

    Comentarios descriptivos de las funciones

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const $content = document.getElementById('mensaje'), $btn = document.getElementB
     o="ober"
     u="ufat" */
 
+    /*Función botón para encriptar */
 function btnEncriptar() {
     const textoEncriptado = encriptar(textArea.value)
     mensaje.value = textoEncriptado
@@ -16,6 +17,7 @@ function btnEncriptar() {
     mensaje.style.backgroundImage = "none"
 }
 
+/**Función para encriptar mensaje */
 function encriptar(stringEncriptado){
     /*let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];*/
     let matrizCodigo = [["e", "-1010-"], ["i", "-011-"], ["a", "-10-"], ["o", "-0-"], ["u", "-0011-"], ["k", "-01-"], ["y", "-000-"], ["d", "-001-"], ["h", "-111-"], ["m", "-100-"], ["w", "-110-"], ["x", "-101-"], ["t", "-010-"], ["j", "-0000-"], ["r", "-0001-"], ["v", "-0111-"], ["s", "-1111-"], ["q", "-1000-"], ["ñ", "-1100-"], ["n", "-1110-"], ["c", "-1001-"], ["g", "-0101-"], ["b", "-0110-"], ["f", "-0100-"], ["p", "-00-"], ["z", "-11-"], ["l", "-1-"]];
@@ -29,12 +31,14 @@ function encriptar(stringEncriptado){
     return stringEncriptado 
 }
 
+/**Función botón desencriptar */
 function btnDesencriptar() {
     const textoEncriptado = desencriptar(textArea.value)
     mensaje.value = textoEncriptado
     textArea.value = "";
 }
 
+/**Función para desencriptar mensaje */
 function desencriptar(stringDesencriptado) {
     /*let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];*/
     let matrizCodigo = [["e", "-1010-"], ["i", "-011-"], ["a", "-10-"], ["o", "-0-"], ["u", "-0011-"], ["k", "-01-"], ["y", "-000-"], ["d", "-001-"], ["h", "-111-"], ["m", "-100-"], ["w", "-110-"], ["x", "-101-"], ["t", "-010-"], ["j", "-0000-"], ["r", "-0001-"], ["v", "-0111-"], ["s", "-1111-"], ["q", "-1000-"], ["ñ", "-1100-"], ["n", "-1110-"], ["c", "-1001-"], ["g", "-0101-"], ["b", "-0110-"], ["f", "-0100-"], ["p", "-00-"], ["z", "-11-"], ["l", "-1-"]];
